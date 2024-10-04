d9cf19bb5ca2229e5c289e3e2f97db40b8a366b1
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2024-10-04 10:01:12 +0700

    praktikum 5

---
+++ hello_world/lib/basic_widget/fab_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class fab_widget extends StatelessWidget {
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          child: const Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
+        ),
+      ),
+    );
+  }
+}
